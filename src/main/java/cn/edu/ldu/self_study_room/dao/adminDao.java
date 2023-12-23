5cd76c9c9c150456ecf577f58ca401d62026241e
xyhaoo <123641146+xyhaoo@users.noreply.github.com>
2023-12-23 10:51:13 +0800

    sout 222

--- src/main/java/cn/edu/ldu/self_study_room/dao/adminDao.java
+++ src/main/java/cn/edu/ldu/self_study_room/dao/adminDao.java
@@ -2,6 +2,6 @@ package cn.edu.ldu.self_study_room.dao;
 
 public class adminDao {
     public static void main(String[] args) {
-        System.out.println("222");
+        System.out.println("2222");
     }
 }
