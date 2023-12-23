a81114cf9cb381d88990ce3f0a7039d20ce18ba2
xyhaoo <123641146+xyhaoo@users.noreply.github.com>
2023-12-23 10:02:08 +0800

    first commit

---
+++ src/main/java/cn/edu/ldu/self_study_room/SelfStudyRoomApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.ldu.self_study_room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfStudyRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfStudyRoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/edu/ldu/self_study_room/SelfStudyRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.ldu.self_study_room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfStudyRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
