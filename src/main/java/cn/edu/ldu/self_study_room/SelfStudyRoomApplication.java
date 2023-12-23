2007ee9baebb0e6ace731edde5eded2a1d16bd8a
xyhaoo <123641146+xyhaoo@users.noreply.github.com>
2023-12-23 10:42:16 +0800

    change xml to yaml

--- src/main/java/cn/edu/ldu/self_study_room/SelfStudyRoomApplication.java
+++ src/main/java/cn/edu/ldu/self_study_room/SelfStudyRoomApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SelfStudyRoomApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SelfStudyRoomApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SelfStudyRoomApplication.class, args);
+    }
 
 }
