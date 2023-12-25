256fb5a8bbc0b979e3125444201758d9b1fe8f69
xyhaoo <123641146+xyhaoo@users.noreply.github.com>
2023-12-25 14:44:49 +0800

    前端模版

---
+++ src/main/java/cn/edu/ldu/self_study_room/admin/controller/TestController.java
@@ -0,0 +1,21 @@
+package cn.edu.ldu.self_study_room.admin.controller;
+
+import cn.edu.ldu.self_study_room.service.NoticeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping(value = "/test")
+public class TestController {
+    @Autowired
+    private NoticeService noticeService;
+
+    @GetMapping()
+    public ModelAndView notice_publish(){
+        return new ModelAndView("index");
+    }
+
+}
